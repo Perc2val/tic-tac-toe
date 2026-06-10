f922eb71a5cbb1d036f54b8b06e0d4dfd4fd319a
Perc2val <fsb099@aol.de>
2026-06-10 21:12:16 +0200

    winning conditions done

--- javascript.js
+++ javascript.js
@@ -29,24 +29,60 @@ const game = (()=> {
     function createPlayer(givenName, marker){
         const playerName = givenName;
         const playerMarker = marker;
+        const playerMarkerPosition = []; 
         const setMarker = function(position){
             if (gameboard.board[position-1]== ""){
                 gameboard.board[position - 1] = marker;
                 console.log(gameboard.board);
-                gameFlow();
+                getPlayerMarkerPosition();
+                checkWinner();
             } else {
                 console.log("You cant place your marker there")
                 console.log(gameboard.board)
             }
         }
+        function getPlayerMarkerPosition(){
+            playerMarkerPosition.length = 0;
+            let idx = gameboard.board.indexOf(playerMarker);
+                while (idx !== -1) {
+                playerMarkerPosition.push(idx);
+                idx = gameboard.board.indexOf(playerMarker, idx + 1);
+            }  
+            console.log(playerMarkerPosition);
+        }
+        function checkWinner(){
+            /*const winningConditions = (["0","1","2"]["0","3","6"]["1","4","7"]["3","4","5"]["2","5","8"]["6","7","8"]["0","4","8"]["6","4","2"])*/
+            const winningConditions ={
+                winOne:[0,1,2],
+                winTwo:[0,3,6],
+                winThree:[1,4,7],
+                winFour:[3,4,5],
+                winFive:[2,5,8],
+                winSix:[6,7,8],
+                winSeven:[0,4,8],
+                winEight:[6,4,2]}
+            if (winningConditions.winOne.toString() == playerMarkerPosition.toString() ||
+                winningConditions.winTwo.toString() == playerMarkerPosition.toString() ||
+                winningConditions.winThree.toString() == playerMarkerPosition.toString() ||
+                winningConditions.winFour.toString() == playerMarkerPosition.toString() ||
+                winningConditions.winFive.toString() == playerMarkerPosition.toString() ||
+                winningConditions.winSix.toString() == playerMarkerPosition.toString() ||
+                winningConditions.winSeven.toString() == playerMarkerPosition.toString() ||
+                winningConditions.winEight.toString() == playerMarkerPosition.toString()){
+                console.log(`${playerName} won with the winning combination ${winningConditions.winOne}`)
+            } else {
+                gameFlow();
+            }
+        }
+
         return {
-            playerName, playerMarker, setMarker,
+            playerName, playerMarker, setMarker, playerMarkerPosition,
         }
     }
 
-    function checkWinner(){
-
-    }
+    
+    
+    
 
     const playerOne = createPlayer("Finn", "X")
     const playerTwo = createPlayer("Charly", "O")
